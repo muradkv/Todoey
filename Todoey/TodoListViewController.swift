f7f2a5dbd824b28013061fce4111135177ed45a9
murad <muradkv@gmail.com>
2022-08-30 11:53:59 +0300

    Add Tableview Data source methods. Show data in table

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Food", "Mood", "Good"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,8 +18,7 @@ class TodoListViewController: UITableViewController {
         configureNavigationBar()
         
     }
-
-
+    
     private func configureNavigationBar() {
         
         let navBarAppearance = UINavigationBarAppearance()
@@ -29,6 +30,23 @@ class TodoListViewController: UITableViewController {
         navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+}
+
+//MARK: - Tableview Datasource Methods
+
+extension TodoListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell") else { return UITableViewCell() }
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
     }
 }
 
