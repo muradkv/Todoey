90376e155943fccb55656008b95029dc7f08406d
murad <muradkv@gmail.com>
2022-08-30 08:59:06 +0300

    Initial commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by murad on 30.08.2022.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
