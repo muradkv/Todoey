5fe8d4711555cf0d8ff3eea5997d7b259f5ceb0c
murad <muradkv@gmail.com>
2024-12-19 16:28:31 +0300

    Initial commit

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by murad on 19.12.2024.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
