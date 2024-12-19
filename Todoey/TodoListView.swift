b8c5aadffd55ba2078f396e29880c08aef53ccca
murad <muradkv@gmail.com>
2024-12-19 17:09:50 +0300

    Setup tableView UI on TodoListView
    - Basic implementation table

---
+++ Todoey/TodoListView.swift
@@ -0,0 +1,54 @@
+//
+//  TodoListView.swift
+//  Todoey
+//
+//  Created by murad on 19.12.2024.
+//
+
+import UIKit
+
+class TodoListView: UIView {
+    
+    //MARK: - Properties
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "defaultCell")
+        return tableView
+    }()
+    
+    //MARK: - Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup UI
+    
+    private func setupView() {
+        addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    //MARK: - Methods
+    
+    func setTableViewDelegate(_ delegate: UITableViewDelegate) {
+        tableView.delegate = delegate
+    }
+    
+    func setTableViewDataSource(_ dataSource: UITableViewDataSource) {
+        tableView.dataSource = dataSource
+    }
+}
