6a8751569d1ffc627c0ee3ac76646b2053999b06
murad <muradkv@gmail.com>
2024-12-19 17:48:49 +0300

    Implement tableViewDelegates to TodoListVC
    - Show mock data in tableView

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class TodoListViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    let todoListView = TodoListView()
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    //MARK: - Life cycle
+    
+    override func loadView() {
+        view = todoListView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+
+        setDelegates()
+    }
+    
+    //MARK: - Methods
+    
+    private func setDelegates() {
+        todoListView.setTableViewDataSource(self)
+        todoListView.setTableViewDataSource(self)
     }
 
 
 }
 
+extension TodoListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        itemArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+}
