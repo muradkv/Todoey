a4840b5e2b79336158d9a814aa5e2e0e771983cb
murad <muradkv@gmail.com>
2024-12-23 03:54:32 +0300

    Add Item model

---
+++ Todoey/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by murad on 21.12.2024.
+//
+
+import Foundation
+
+struct Item {
+    var title: String
+    var done: Bool
+}
--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,7 +12,10 @@ class TodoListViewController: UIViewController {
     //MARK: - Properties
     
     let todoListView = TodoListView()
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [
+        Item(title: "Find Mike", done: false),
+        Item(title: "Buy Eggos", done: true),
+        Item(title: "Destroy Demogorgon", done: false)]
     let defaults = UserDefaults()
     
     //MARK: - Life cycle
@@ -24,12 +27,14 @@ class TodoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //let dataFile = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        
         configureNavBar()
         setDelegates()
         setupAddButton()
         
-        if let item = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = item 
+        if let item = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = item
         }
     }
     
@@ -60,8 +65,9 @@ class TodoListViewController: UIViewController {
         }
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            self.itemArray.append(textField.text!)
-            
+            let newItem = Item(title: textField.text!, done: false)
+            self.itemArray.append(newItem)
+                    
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.todoListView.reloadTableView()
@@ -81,7 +87,12 @@ extension TodoListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -89,10 +100,8 @@ extension TodoListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.none {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        todoListView.reloadTableView()
     }
 }
